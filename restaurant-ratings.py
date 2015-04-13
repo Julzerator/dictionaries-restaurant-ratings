d7dc054a6c7ee36c1d454cbdb1f4b0b0c1806a3f
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 15:32:17 -0700

    Code reviewed and added notes to improve future code.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -5,17 +5,26 @@ def rating_sort(scores_filepath):
 
     for restaurant in scores:
         restaurant = restaurant.rstrip()
+        #This will work, but scorelist is confusing as a name here:
         scorelist = restaurant.split(':')
-        # restaurantname, score
         name, score = scorelist
-        restaurantscores[name] = score
 
-    alpha_restaurants = sorted(restaurantscores)
+        # This is a single line to do what is the two lines previously:
+        # name, score = restaurant.split(':')
+
+        # Change the strings of numbers into int as soon as you can 
+        # to prevent issues later on: 
+        restaurantscores[name] = int(score)
+
+    # To remind yourself that sorted returns a list of the key values
+    # add .keys() to the dictionary when calling it.
+    alpha_restaurants = sorted(restaurantscores.keys())
 
     for name in alpha_restaurants:
         score = restaurantscores.get(name, "does not exist")
         print "Restaurant %s is rated at %s." % (name, score)
 
+    
 
 
 rating_sort("scores.txt")
